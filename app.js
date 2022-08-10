7afe5f5c811b6a458480ced0ac88e53cc39e9d59
DamianDuarte <106710013+DamianDuarte@users.noreply.github.com>
2022-08-10 18:24:07 -0300

    ahora si funciono con dos controladores

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mainRouter = require('./routes/main')
+var indexRouter = require('./routes/index');
+var aboutRouter = require('./routes/about')
 
 var app = express();
 
@@ -18,7 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', mainRouter);
+app.use('/', indexRouter);
+app.use('/about', aboutRouter);
 
 
 // catch 404 and forward to error handler
--- controllers/mainController.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    index: (req, res)=>{
-        return res.render('index')
-    },
-    about: (req, res)=>{
-        return res.render('about')
-    }
-}
